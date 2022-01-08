301d89d4fa31f827c9df0b756e3e859e3658f145
Marco Preciado <mac46005@gmail.com>
2022-01-08 08:19:18 -0600

    Testing Enter pressed on text box

--- TheNoteTaker_WPF_UI/MVVM/ViewModels/NoteViewModel.cs
+++ TheNoteTaker_WPF_UI/MVVM/ViewModels/NoteViewModel.cs
@@ -1,13 +1,52 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TheNoteTaker_WPF_UI.MVVM.Models;
 
 namespace TheNoteTaker_WPF_UI.MVVM.ViewModels
 {
     public class NoteViewModel : Screen
     {
+        private ObservableCollection<NoteModel> _listofNotes;
+
+        public ObservableCollection<NoteModel> ListOfNotes
+        {
+            get { return _listofNotes; }
+            set 
+            {
+                _listofNotes = value;
+                NotifyOfPropertyChange(() => ListOfNotes);
+            }
+        }
+
+        private string _noteTextBox;
+
+        public string NoteTextBox
+        {
+            get { return _noteTextBox; }
+            set 
+            {
+                _noteTextBox = value;
+                NotifyOfPropertyChange(() => NoteTextBox);
+            }
+        }
+
+
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Console.WriteLine();
+            }
+        }
     }
+
+
+
 }
