cb23b957d106d8b4dd90ef9494e50d6dbbabf20b
Marco Preciado <mac46005@gmail.com>
2022-02-14 06:00:26 -0600

    Added property changed

---
+++ TheNoteTaker/TNT_WPF/Core/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNT_WPF.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChaged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
